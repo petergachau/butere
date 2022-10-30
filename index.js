a1a9aca4d02fcfee9176d0fa878a11b5c48cd2d3
petergachau <83207831+petergachau@users.noreply.github.com>
2022-10-30 15:16:42 +0300

    first commit

--- index.js
+++ index.js
@@ -13,14 +13,11 @@ const app = express();
 
 // db connection
 const port = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.DATABASE)
-  .then(() => {
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-  })
-  .catch((error) => console.log(`${error} did not connect`));
-
+app.get("/", (req, res) => {
+    return res.json({
+      data: "hello world from kaloraat auth API",
+    });
+  });
 // middlewares
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
