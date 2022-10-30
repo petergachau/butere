d1866bf74d9d640c903ef85ad7f88bd056b6ba0f
petergachau <83207831+petergachau@users.noreply.github.com>
2022-10-30 15:12:27 +0300

    first commit

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ import morgan from 'morgan'
 const app = express();
 
 // db connection
+const port = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.DATABASE)
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log("DB CONNECTION ERROR: ", err));
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
 
 // middlewares
 app.use(express.json({ limit: "4mb" }));
